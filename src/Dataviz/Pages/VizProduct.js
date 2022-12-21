e346fed5fdd7da9656cafa756e10bd6bfaa0e23c
Aravind R <raaravind@kaartech.com>
2022-12-21 17:34:14 +0530

    img-1

--- src/Dataviz/Pages/VizProduct.js
+++ src/Dataviz/Pages/VizProduct.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 const VizProduct = () => {
   const { id } = useParams();
-  const [data_temp, setDataTemp] = useState([]);
+  const [data_temp, setDataTemp] = useState({});
   useEffect(() => {
     fetch(`http://localhost:8000/vizprod/${id}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        setDataTemp(data.message);
       });
   }, []);
 
@@ -16,6 +17,11 @@ const VizProduct = () => {
       <h1 class="h-1" style={{ color: "white", margin: "60px" }}>
         Hello product {id}
       </h1>
+      {Object.keys(data_temp).length > 0 ? (
+        <img src={data_temp.urls[0].link} alt="" height={1000} width={1000} />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
