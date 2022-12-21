5e816876942f80aded3e2ae7ec1e417e2fef8c6e
Aravind R <raaravind@kaartech.com>
2022-12-21 16:59:34 +0530

    back tiles

--- src/Dataviz/Pages/VizProduct.js
+++ src/Dataviz/Pages/VizProduct.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 const VizProduct = () => {
   const { id } = useParams();
+  const [data_temp, setDataTemp] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:8000/vizprod/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   return (
     <div class="flex flex-row align-middle justify-center">
