7b93382d75437fbdaddc1b76e44cd78ce1ace11d
Aravind R <raaravind@kaartech.com>
2022-12-22 11:49:33 +0530

    new done

--- src/Dataviz/Pages/VizProduct.js
+++ src/Dataviz/Pages/VizProduct.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
 const VizProduct = () => {
   const { id } = useParams();
   const [data_temp, setDataTemp] = useState({});
