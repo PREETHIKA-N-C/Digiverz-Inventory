9d7a89112bc05ec0418ae986fdce08e3f46a8856
Aravind R <raaravind@kaartech.com>
2022-12-22 15:02:12 +0530

    grouped

--- src/Dataviz/Pages/VizHome.js
+++ src/Dataviz/Pages/VizHome.js
@@ -16,6 +16,7 @@ const VizHome = () => {
   const theme = useSelector(ThemeState);
   const dispatch = useDispatch();
   const [newdata, setNewdata] = useState([]);
+  const [group, setGroup] = useState([]);
   const [load, setLoad] = useState(true);
 
   useEffect(() => {
@@ -23,7 +24,13 @@ const VizHome = () => {
       .then((response) => response.json())
       .then((data) => {
         setNewdata(data.message);
-        setLoad(false);
+        fetch("http://localhost:8000/viz/group")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setGroup(data.message);
+            setLoad(false);
+          });
       });
   }, []);
 
@@ -33,30 +40,54 @@ const VizHome = () => {
     document.body.style.background = "#121212";
   }
   return (
-    <div className="grid grid-cols-3 grid-flow-row  pl-24 pt-20 pb-20 overflow-auto h-screen alfa">
+    <div>
       {load ? (
-        <>
+        <div className="grid grid-cols-3 grid-flow-row  pl-24 pt-20 pb-20">
           <ProductCardLoader />
           <ProductCardLoader />
           <ProductCardLoader />
-        </>
+        </div>
       ) : (
-        <>
-          {newdata.map((item) => {
+        <div>
+          {group.map((item) => {
             return (
-              <div key={item.id} style={{ margin: "20px" }}>
-                <NavLink to={`./Product/${item.id}`}>
-                  <ProductCard
-                    name={item.name}
-                    desc={item.desc}
-                    img={item.img}
-                    logo={item.logo}
-                  />
-                </NavLink>
+              <div key={item.id}>
+                <h2
+                  style={{
+                    color: "white",
+                    textAlign: "center",
+                    padding: "5px",
+                    position: "relative",
+                    top: "70px",
+                  }}
+                  className="bg-slate-700"
+                >
+                  {item.group}
+                </h2>
+                <div className="grid grid-cols-3 grid-flow-row  pl-24 pt-20 pb-20 mt-12 -mb-16">
+                  {newdata
+                    .filter((item2) => {
+                      return item2.desc === item.group;
+                    })
+                    .map((item) => {
+                      return (
+                        <div key={item.id}>
+                          <NavLink to={`./Product/${item.id}`}>
+                            <ProductCard
+                              name={item.name}
+                              desc={item.desc}
+                              img={item.img}
+                              logo={item.logo}
+                            />
+                          </NavLink>
+                        </div>
+                      );
+                    })}
+                </div>
               </div>
             );
           })}
-        </>
+        </div>
       )}
     </div>
   );
