270dafac385b6f9cbdb4775bc021691f16636377
Harishcruise <harishbarath2001@gmail.com>
2023-01-03 11:42:01 +0530

    responsive

--- src/Dataviz/Components/SliderCarousel.js
+++ src/Dataviz/Components/SliderCarousel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import {
   Light,
   Dark,
